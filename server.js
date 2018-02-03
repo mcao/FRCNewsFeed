e9c91bdb337ebbe7ad7cf1b56e7edbd48d689647
Michael Cao <mike.x.cao@gmail.com>
2018-02-03 17:01:40 -0500

    events no work

--- server.js
+++ server.js
@@ -22,68 +22,68 @@ class Server extends EventEmitter {
 
     app.post('/chiefdelphi', function (req, res) {
       res.send('OK');
-      this.emitEvent('chiefdelphi', req.body);
+      emitEvent('chiefdelphi', req.body);
     })
 
     app.post('/tba', function (req, res) {
       res.send('OK');
-      this.emitEvent('tba', req.body);
+      emitEvent('tba', req.body);
     })
 
     app.post('/frcqa', function (req, res) {
       res.send('OK');
-      this.emitEvent('frcqa', req.body);
+      emitEvent('frcqa', req.body);
     })
 
     app.post('/frcblog', function (req, res) {
       res.send('OK');
-      this.emitEvent('frcblog', req.body);
+      emitEvent('frcblog', req.body);
     })
 
     // No functionality yet
     app.post('/toa', function (req, res) {
       res.send('OK');
-      this.emitEvent('toa', req.body);
+      emitEvent('toa', req.body);
     })
 
     // Public
     app.post('/reddit', function (req, res) {
       res.send('OK');
-      this.emitEvent('reddit', req.body);
+      emitEvent('reddit', req.body);
     })
 
     app.post('/twitch', function (req, res) {
       res.send('OK');
-      this.emitEvent('twitch', req.body);
+      emitEvent('twitch', req.body);
     })
 
     app.post('/yt', function (req, res) {
       res.send('OK');
-      this.emitEvent('yt', req.body);
+      emitEvent('yt', req.body);
     })
 
     app.post('/twitter', function (req, res) {
       res.send('OK');
-      this.emitEvent('twitter', req.body);
+      emitEvent('twitter', req.body);
     })
 
     app.post('/instagram', function (req, res) {
       res.send('OK');
-      this.emitEvent('instagram', req.body);
+      emitEvent('instagram', req.body);
     })
 
     app.post('/facebook', function (req, res) {
       res.send('OK');
-      this.emitEvent('facebook', req.body);
+      emitEvent('facebook', req.body);
     })
 
     app.get('/', (req, res) => res.send('hello world'))
 
     console.log('Listeners initialized!')
-  }
 
-  emitEvent(type, content) {
-    this.emit(type, content);
+    function emitEvent(type, content) {
+      this.emit(type, content);
+    }
   }
 }
 
