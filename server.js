4c77158dce4c454396ace45085c43b1127132e76
Michael Cao <mike.x.cao@gmail.com>
2018-02-03 16:57:07 -0500

    test events

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ class Server extends EventEmitter {
 
     app.post('/chiefdelphi', function (req, res) {
       res.send('OK');
-      this.emitEvent('chiefdelphi', req.body);
+      this.this.emitEvent('chiefdelphi', req.body);
     })
 
     app.post('/tba', function (req, res) {
