f9397369e1b0023b9abdf6bf95a66d875a5573c7
Michael Cao <mike.x.cao@gmail.com>
2018-02-02 19:08:15 -0500

    added more options

--- server.js
+++ server.js
@@ -7,12 +7,48 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
+// FRC Q and A, TOA, /r/FRC, Twitch, YouTube, FRC Blog
+
 app.post('/chiefdelphi', function (req, res) {
   console.log(req.body);
+  res.send('ok');
 })
 
 app.post('/tba', function (req, res) {
   console.log(req.body);
+  res.send('ok');
+})
+
+app.post('/frcqa', function (req, res) {
+  console.log(req.body);
+  res.send('ok');
+})
+
+app.post('/frcblog', function (req, res) {
+  console.log(req.body);
+  res.send('ok');
+})
+
+// No functionality yet
+app.post('/toa', function (req, res) {
+  console.log(req.body);
+  res.send('ok');
+})
+
+app.post('/reddit', function (req, res) {
+  console.log(req.body);
+  res.send('ok');
+})
+
+app.post('/twitch', function (req, res) {
+  console.log(req.body);
+  res.send('ok');
+})
+
+// No functionality yet
+app.post('/yt', function (req, res) {
+  console.log(req.body);
+  res.send('ok');
 })
 
 app.get('/', (req, res) => res.send('hello world'))
