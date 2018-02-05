041ce95674f8c61637099e9c71c4b00ba6eff975
Michael Cao <mike.x.cao@gmail.com>
2018-02-05 18:39:31 -0500

    it ain't executing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-  app = express.createServer(),
+  app = express(),
   bodyParser = require('body-parser'),
   { EventEmitter } = require('events')
 var token = "",
