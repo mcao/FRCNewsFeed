5c65c75605e7d9c0af22f751a7c29d54e737d2d3
Michael Cao <mike.x.cao@gmail.com>
2018-02-05 18:49:01 -0500

    raw body

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ class Server extends EventEmitter {
   constructor() {
     super();
 
-    app.use(bodyParser.text({ type: '*/*' }));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(function (req, res, next) {
+      req.rawBody = '';
+      req.setEncoding('utf8');
+      req.on('data', function (chunk) { req.rawBody += chunk });
+      next();
+    });
 
     app.listen(8080, () => {
       console.log('Server started at port 8080!')
